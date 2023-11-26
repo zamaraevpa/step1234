5fdf25f9c5f25ac4ceef41a4ed1b7887b72ed50a
kottik-mypp <kostiktakaya@gmail.com>
2023-11-26 15:20:03 +0300

    Add postgresql support

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -5,7 +6,14 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy()
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+sql_user = os.environ["SQL_USER"]
+sql_password = os.environ["SQL_PASSWORD"]
+sql_host = os.environ["SQL_HOST"]
+sql_port = os.environ["SQL_PORT"]
+sql_database = os.environ["SQL_DATABASE"]
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{sql_user}:{sql_password}@{sql_host}:{sql_port}/{sql_database}"
 app.config["SECRET_KEY"] = secrets.token_hex()
 
 db.init_app(app)
