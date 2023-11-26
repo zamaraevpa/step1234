789dba7f4602c996a2cb62500f6bcc0c5ba80ef2
kottik-mypp <kostiktakaya@gmail.com>
2023-11-26 15:44:51 +0300

    Add support sqlite and postgres in one app

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -6,14 +6,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy()
 
-sql_user = os.environ["SQL_USER"]
-sql_password = os.environ["SQL_PASSWORD"]
-sql_host = os.environ["SQL_HOST"]
-sql_port = os.environ["SQL_PORT"]
-sql_database = os.environ["SQL_DATABASE"]
+if "SQL_DATABASE_TYPE" in os.environ and os.environ["SQL_DATABASE_TYPE"] == "postgres":
+    app.config["SQLALCHEMY_DATABASE_URI"] = get_postgres_database_uri()
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{sql_user}:{sql_password}@{sql_host}:{sql_port}/{sql_database}"
 app.config["SECRET_KEY"] = secrets.token_hex()
 
 db.init_app(app)
@@ -29,3 +26,11 @@ app.register_blueprint(auth.bp)
 from . import views
 app.register_blueprint(views.bp)
 app.add_url_rule('/', endpoint='index')
+
+def get_postgres_database_uri():
+    sql_user = os.environ["SQL_USER"]
+    sql_password = os.environ["SQL_PASSWORD"]
+    sql_host = os.environ["SQL_HOST"]
+    sql_port = os.environ["SQL_PORT"]
+    sql_database = os.environ["SQL_DATABASE"]
+    return f"postgresql://{sql_user}:{sql_password}@{sql_host}:{sql_port}/{sql_database}"
